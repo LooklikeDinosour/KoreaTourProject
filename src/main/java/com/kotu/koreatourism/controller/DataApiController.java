a81eadbccf86f6beb1cfd7f1e7f65675fa5a9fe5
DinoKim <133090170+LooklikeDinosour@users.noreply.github.com>
2024-05-22 03:10:53 +0900

    240522 공공API 일부 불러오는 것은 테스트 성공

--- src/main/java/com/kotu/koreatourism/controller/DataApiController.java
+++ src/main/java/com/kotu/koreatourism/controller/DataApiController.java
@@ -1,14 +1,29 @@
 package com.kotu.koreatourism.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@Controller
-@RequestMapping
-public class DataApiController {
+//@Slf4j
+//@RestController
+//@RequestMapping("/api")
+//public class DataApiController {
+//
+//    @Value("${callbackurl.kor}")
+//    private String callBackUrl;
+//
+//    @Value("${openapiservicekey}")
+//    private String ServiceKey;
+//
+//    @GetMapping("/tour")
+//    public ResponseEntity<String> tourInfo() {
+//
+//        return null;
+//    }
 
-}
+//}
