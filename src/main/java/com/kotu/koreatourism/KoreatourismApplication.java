1b3fdbc7654a951066803ac5cb9ac0cc66521099
DinoKim <133090170+LooklikeDinosour@users.noreply.github.com>
2024-08-23 18:04:11 +0900

    mybatis 에러 발생 하여 debug-> trace로 변경 및 @Mapperscan 추가

--- src/main/java/com/kotu/koreatourism/KoreatourismApplication.java
+++ src/main/java/com/kotu/koreatourism/KoreatourismApplication.java
@@ -1,9 +1,11 @@
 package com.kotu.koreatourism;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kotu.koreatourism.mapper")
 public class KoreatourismApplication {
 
     public static void main(String[] args) {
