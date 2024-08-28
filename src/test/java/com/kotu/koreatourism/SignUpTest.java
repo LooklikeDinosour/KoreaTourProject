f984cba8d501e1b0734302c3431428288167415f
DinoKim <133090170+LooklikeDinosour@users.noreply.github.com>
2024-08-28 15:46:37 +0900

    회원가입 오류 해결 못해서 Test 도입

---
+++ src/test/java/com/kotu/koreatourism/SignUpTest.java
@@ -0,0 +1,58 @@
+package com.kotu.koreatourism;
+
+import com.kotu.koreatourism.dto.LoginDTO;
+import com.kotu.koreatourism.dto.SignUpFormDTO;
+import com.kotu.koreatourism.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class SignUpTest {
+    @Autowired
+    UserService userService;
+
+    @DisplayName("회원가입 및 검증 테스트")
+    @Test
+    public void signUpTest() {
+
+        //given
+        SignUpFormDTO newUser = new SignUpFormDTO();
+        newUser.setUserId("kotutest12345");
+        newUser.setUserPassword("Kotutest12345!");
+        newUser.setUserEmail("test@mail.com");
+        newUser.setUserPhone("010-2342-4343");
+        newUser.setUserNickname("테스트1");
+
+        //when
+        int result = userService.signUp(newUser);
+
+        //then
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("중복확인 테스트")
+    @Test
+    public void duplicatedTest() {
+
+        //given
+        SignUpFormDTO newUser = new SignUpFormDTO();
+        newUser.setUserId("kotutest12345");
+        newUser.setUserPassword("Kotutest12345!");
+        newUser.setUserEmail("test@mail.com");
+        newUser.setUserPhone("010-2342-4343");
+        newUser.setUserNickname("테스트1");
+        userService.signUp(newUser);
+
+        //when
+        LoginDTO newLoginDTO = userService.findByUserId("kotutest12345");
+
+        //then
+        Assertions.assertThat(newLoginDTO.getUserId()).isEqualTo(newUser.getUserId());
+    }
+
+}
