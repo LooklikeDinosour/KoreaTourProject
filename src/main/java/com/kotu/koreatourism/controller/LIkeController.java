e075d31a5c9e63aae2f515b672aef0484211089e
DinoKim <133090170+LooklikeDinosour@users.noreply.github.com>
2025-02-14 04:41:31 +0900

    Add 좋아요 내역 확인

--- src/main/java/com/kotu/koreatourism/controller/LIkeController.java
+++ src/main/java/com/kotu/koreatourism/controller/LIkeController.java
@@ -1,5 +1,6 @@
 package com.kotu.koreatourism.controller;
 
+import com.kotu.koreatourism.dto.LikeDTO;
 import com.kotu.koreatourism.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,11 +10,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -25,25 +25,42 @@ public class LIkeController {
     private final LikeService likeService;
 
     @PostMapping("/switch")
-    public ResponseEntity<String> toggleLike(@RequestBody Map<String, Integer> request, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<Map<String, Object>> toggleLike(@RequestBody Map<String, Integer> request,
+                                                          @AuthenticationPrincipal UserDetails userDetails) {
+
+        Map<String, Object> response = new HashMap<>();
+
         try {
             log.info("좋아요 요청");
             String userId = userDetails.getUsername();
             int contentId = request.get("contentId");
 
             if (userId == null || contentId == 0) {
-                return ResponseEntity.badRequest().body("잘못된 요청입니다.");
+                response.put("isLiked", false);
+                response.put("message", "잘못된 요청입니다.");
+                return ResponseEntity.badRequest().body(response);
             }
 
             boolean isLiked = likeService.toggleLike(userId, contentId);
 
             log.info(isLiked ? "좋아요 완료" : "좋아요 취소");
-            return ResponseEntity.ok(isLiked ? "좋아요 완료!" : "좋아요 취소!");
+            response.put("isLiked", isLiked);
+            response.put("message", isLiked ? "좋아요 완료" : "좋아요 취소");
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
             log.error("좋아요 작동 중 오류 발생 = {}", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("좋아요 처리 중 오류가 발생했습니다.");
+            response.put("isLiked", false);
+            response.put("message", "좋아요 처리 중 오류가 발생했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<List<LikeDTO>> getLikesList(@AuthenticationPrincipal UserDetails userDetails) {
+        String userId = userDetails.getUsername();
+        likeService.getLikeList(userId);
+
+
+    }
 
 }
